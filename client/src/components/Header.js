5d4de80c6983db08cdfedef1f4c8f48f312fc148
Hyun Soo <junghss0113@gmail.com>
2022-03-19 00:39:58 +0900

    css

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,10 +1,51 @@
 import { Nav } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import s from '../css/Header.module.css'
 //상단 내비게이션
 const Header = () => {
     
     return (
-        <div className='Header'>
-            <Nav fill variant="tabs" defaultActiveKey="/" className='nav'>         
+        <div className={s.Header}>
+            <nav className={s.nav}>
+                <a href='#' className={s.nav_logo}>Let's Be CEO</a>
+
+                <div className={s.nav_menu}>
+                    <ul className={s.nav_list}>
+                        <li className={s.nav_item}>
+                            <a href='#' className={s.nav_link}>상권분석</a>
+                        </li>
+                    </ul>
+                    <ul className={s.nav_list}>
+                        <li className={s.nav_item}>
+                            <a href='#' className={s.nav_link}>손익분기점</a>
+                        </li>
+                    </ul>
+                    <ul className={s.nav_list}>
+                        <li className={s.nav_item}>
+                            <a href='#' className={s.nav_link}>게시판</a>
+                        </li>
+                    </ul>
+                    <ul className={s.nav_list}>
+                        <li className={s.nav_item}>
+                            <a href='#' className={s.nav_link}>CONTACT</a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+
+
+
+
+
+
+
+
+
+
+
+
+
+            {/* <Nav fill variant="tabs" defaultActiveKey="/" className='nav'>         
                 <Nav.Item>
                     <Nav.Link href="/">LOGO</Nav.Link>
                 </Nav.Item>
@@ -16,7 +57,7 @@ const Header = () => {
                 </Nav.Item>
                 <Nav.Item>
                     <Nav.Link href="/board">게시판</Nav.Link>
-                </Nav.Item>
+                </Nav.Item> */}
                 {/* <Nav.Item>
                     <Nav.Link eventKey="home">게시판</Nav.Link>
                 </Nav.Item> */}
@@ -25,7 +66,7 @@ const Header = () => {
                         Disabled
                     </Nav.Link>
                 </Nav.Item> */}      
-            </Nav>       
+            {/* </Nav>        */}
         </div>
     );
 };
